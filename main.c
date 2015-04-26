f5b8e99b40d5eff5cc60378a7a29d655cc2a6af9
Yan Zhang <cyanogenoid@cyanogenoid.com>
2015-04-26 20:24:55 +0100

    Integrate RSA encryption and decryption

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "os.h"
+#include <string.h>
 
 
 int check_switches(int);
@@ -18,7 +19,88 @@ void main(void) {
 int check_switches(int state) {
 
     if (get_switch_press(_BV(SWC))) {
-        send_text(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~");
+        // TODO load from EEPROM
+        // make key
+        bigint_word_t e_val = 17;
+        uint16_t n_val = 3233;
+        bigint_word_t n_arr[] = {n_val & 0xFF, n_val >> 8};
+        uint16_t d_val = 2753;
+        bigint_word_t d_arr[] = {d_val & 0xFF, d_val >> 8};
+
+
+        bigint_t e = {
+            .length_W = 1,
+            .info = 0,
+            .wordv = &e_val,
+        };
+        bigint_t n = {
+            .length_W = 2,
+            .info = 0,
+            .wordv = n_arr,
+        };
+        bigint_t d = {
+            .length_W = 2,
+            .info = 0,
+            .wordv = d_arr,
+        };
+
+        bigint_adjust(&e);
+        bigint_adjust(&n);
+        bigint_adjust(&d);
+
+        rsa_publickey_t pub_key = {
+            .exponent = e,
+            .modulus = n,
+        };
+        rsa_privatekey_t priv_key= {
+            .n = 1,
+            .modulus = n,
+            .components = &d,
+        };
+
+
+        char* secret = "O";
+        bigint_length_t bytes = 1+1;
+        char* data_string = (char*) malloc(bytes*sizeof(char));
+        strcpy(data_string, secret);
+
+        bigint_t data = {
+            .length_W = bytes-1,
+            .info = 0,
+            .wordv = (bigint_word_t*) data_string,
+        };
+        bigint_adjust(&data);
+
+        display_string("1: ");
+        display_string((char*)(data.wordv)); // plaintext
+        display_string("\n");
+
+        display_uint16(data.wordv[0]);
+        display_char('_');
+        display_uint16(data.wordv[1]);
+        display_string("\n");
+
+        rsa_enc(&data, &pub_key);
+        display_string("2: ");
+        display_string((char*)(data.wordv)); // encrypted
+        display_string("\n");
+
+        display_uint16(data.wordv[0]);
+        display_char('_');
+        display_uint16(data.wordv[1]);
+        display_string("\n");
+
+        rsa_dec(&data, &priv_key);
+        display_string("3: ");
+        display_string((char*)(data.wordv)); // decrypted
+        display_string("\n");
+
+        display_uint16(data.wordv[0]);
+        display_char('_');
+        display_uint16(data.wordv[1]);
+        display_string("\n");
+
     }
 
+    return state;
 }

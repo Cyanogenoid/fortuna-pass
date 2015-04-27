94acf42551c26acb6510c1922e44fb5097e9a8c1
Yan Zhang <cyanogenoid@cyanogenoid.com>
2015-04-27 23:23:31 +0100

    Make crypto compile

--- crypto/crypto.c
+++ crypto/crypto.c
@@ -1,31 +1,54 @@
 #include "crypto.h"
 
 #include <avr/eeprom.h>
+#include <stdlib.h>
 
+#define PRIVATE_KEY "\x00"
+#define PRIVATE_KEY_LENGTH_BIT 2048
+#define PRIVATE_KEY_LENGTH_BYTE PRIVATE_KEY_LENGTH_BIT/8
+#define MODULUS 3233
 
-#define PRIVATE_KEY "\x"
-#define PRIVATE_KEY_LENGTH 2048
-
+char eep_priv_key[PRIVATE_KEY_LENGTH_BYTE] EEMEM = PRIVATE_KEY;
 static rsa_privatekey_t priv_key;
-char eep_priv_key[PRIVATE_KEY_LENGTH] EEMEM = (char*) PRIVATE_KEY;
-
+static bigint_t modulus = {
+    .length_W = 2,
+    .info = 0,
+    .wordv = {MODULUS & 0xFF, MODULUS >> 8},
+};
+static bigint_t priv_d;
 
 void load_private_key(char* key) {
-	// load
-	size_t length = PRIVATE_KEY_LENGTH/8/sizeof(char);
-	char* buffer = (char*) malloc(length);
-	eeprom_read_block(buffer, eep_priv_key, length);
-	// decrypt with AES
+    // load key
+    char* buffer = (char*) malloc(PRIVATE_KEY_LENGTH_BYTE);
+    eeprom_read_block(buffer, eep_priv_key, PRIVATE_KEY_LENGTH_BYTE);
+    // decrypt with AES
     aes256_ctx_t context = {{0}};
     aes256_init(key, &context);
     size_t i;
-    for (i = 0; i < length; i += 16) {
-		aes256_dec(buffer[i], &context);
-	}
-	// make into bigint
-
+    for (i = 0; i < PRIVATE_KEY_LENGTH_BYTE; i += 16) {
+        aes256_dec(buffer+i, &context);
+    }
+    // make into bigint
+    bigint_t local_priv_d = {
+        .length_W = PRIVATE_KEY_LENGTH_BYTE,
+        .wordv = buffer,
+    };
+    priv_d = local_priv_d;
+    bigint_adjust(&priv_d);
+    // put into private key
+    rsa_privatekey_t local_priv_key = {
+        .n = 1,
+        .modulus = modulus,
+        .components = &priv_d,
+    };
+    priv_key = local_priv_key;
 }
 
 void decrypt(void* buffer, size_t length_B) {
-
+    bigint_t data = {
+        .length_W = length_B,
+        .wordv = buffer,
+    };
+    bigint_adjust(&data);
+    rsa_dec(&data, &priv_key);
 }

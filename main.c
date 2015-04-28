25ac76f8ebf420c58d5b7779522f3002517d0d0b
Yan Zhang <cyanogenoid@cyanogenoid.com>
2015-04-28 13:01:31 +0100

    Add waiting for SD card state

--- main.c
+++ main.c
@@ -5,17 +5,19 @@
 #define STATE_LOGIN 0
 #define STATE_BROWSE 1
 #define STATE_UNLOCK 2
+#define STATE_SD_WAIT 3
 
 
 int transition(int);
 int process_login(void);
 int process_browse(void);
 int process_unlock(void);
+int process_sd_wait(void);
 
 void main(void) {
     os_init();
 
-    os_add_task( transition,  100, STATE_LOGIN);
+    os_add_task( transition,  100, STATE_SD_WAIT);
 
     sei();
     for(;;){}
@@ -30,8 +32,18 @@ int transition(int state) {
             return process_browse();
         case STATE_UNLOCK:
             return process_unlock();
+        case STATE_SD_WAIT:
+            return process_sd_wait();
     }
 
+}
+
+int process_sd_wait(void) {
+    if (get_switch_long(_BV(OS_CD))) {
+        return STATE_LOGIN;
+    }
+
+    return STATE_SD_WAIT;
 }
 
 int process_login(void) {
